a30646330d303f74960770d256b10eed0538fa22
suleman15 <sulemanahmed2704@gmail.com>
2024-03-04 21:48:31 -0800

    project.jsxFixed

--- components/custom/Hero.jsx
+++ components/custom/Hero.jsx
@@ -69,13 +69,7 @@ const Hero = () => {
         >
           <span>Hi I'm </span>
           <span className="  text-5xl font-bold">
-            <span
-              before="Suleman"
-              className={`relative before:content-[''] before:block before:absolute before:bottom-[-2px] before:rounded-full before:bg-gradient-to-r before:from-primary/50 before:to-primary/30 before:w-full before:h-2 before:blur-md`}
-            >
-              Suleman
-            </span>{" "}
-            Ahmed
+            <span before="Suleman">Suleman</span> Ahmed
           </span>
           <span className="mt-5">
             Full Stack Developer,UI/UX Designer and Engineer
