9e90d358c08bac4bb8cfd2e624393eb613e519e8
suleman15 <sulemanahmed2704@gmail.com>
2024-03-05 19:40:12 -0800

    somethingAdded

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,56 +9,8 @@ module.exports = {
   ],
   prefix: "",
   theme: {
-    typography: (theme) => ({
-      default: {
-        css: {
-          pre: {
-            color: theme("colors.grey.1000"),
-            backgroundColor: theme("colors.grey.100"),
-          },
-          "pre code::before": {
-            "padding-left": "unset",
-          },
-          "pre code::after": {
-            "padding-right": "unset",
-          },
-          code: {
-            backgroundColor: theme("colors.grey.100"),
-            color: "#DD1144",
-            fontWeight: "400",
-            "border-radius": "0.25rem",
-          },
-          "code::before": {
-            content: '""',
-            "padding-left": "0.25rem",
-          },
-          "code::after": {
-            content: '""',
-            "padding-right": "0.25rem",
-          },
-        },
-      },
-    }),
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
-      // typography: {
-      //   DEFAULT: {
-      //       css: {
-      //           "code::before": {content: ''},
-      //           "code::after": {content: ''}
-      //       }
-      //   }},
       colors: {
-        grey: {
-          100: "#F5F7FA",
-          1000: "#1F2933",
-        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
