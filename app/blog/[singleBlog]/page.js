97dfa789f792442c5e32efa139784ad089f743ae
suleman15 <sulemanahmed2704@gmail.com>
2024-03-22 20:55:37 -0700

    done some fixes

--- app/blog/[singleBlog]/page.js
+++ app/blog/[singleBlog]/page.js
@@ -7,11 +7,25 @@ export const revalidate = 30; // revalidate at most 30 seconds
 async function getData(slug) {
   const query = `
     *[_type == "blog" && slug.current == '${slug}'] {
-        "currentSlug": slug.current,
-          title,
-          content,
-          titleImage
-      }[0]`;
+      "currentSlug": slug.current,
+      title,
+      content[]{
+        ...,
+        _type == "image" => {
+          "url": asset->url,
+          "alt": alt
+        }
+      },
+      titleImage {
+        asset-> {
+          url,
+          metadata {
+            dimensions
+          }
+        },
+        alt
+      }
+    }[0]`;
 
   const data = await client.fetch(query);
   return data;
@@ -19,37 +33,36 @@ async function getData(slug) {
 
 export default async function BlogArticle({ params }) {
   const data = await getData(params.singleBlog);
-  console.log(data.content);
+  console.log(data);
   return (
     <div className="mt-24">
-      <h1>
-        <span className="block text-base text-center text-primary font-semibold tracking-wide uppercase">
+      <h1 className="text-center">
+        <span className="block text-base text-primary font-semibold tracking-wide uppercase">
           Suleman Ahmed - Blog
         </span>
-        <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">
+        <span className="mt-2 block text-3xl leading-8 font-bold tracking-tight sm:text-4xl">
           {data.title}
         </span>
       </h1>
 
-      <div className="px-2">
+      <div className="px-2 max-w-2xl mx-auto">
         <Image
-          src={urlFor(data.titleImage).url()}
-          width={800}
-          height={800}
-          alt="Title Image"
+          src={data.titleImage.asset.url}
+          width={data.titleImage.asset.metadata.dimensions.width}
+          height={data.titleImage.asset.metadata.dimensions.height}
+          alt={data.titleImage.alt}
           priority
-          className="rounded-lg  mt-8 border mx-auto"
+          className="rounded-lg mt-8 border mx-auto"
         />
       </div>
-      <div className="mt-16 max-w-5xl lg:ml-5 max-md:px-2 prose prose-blue  prose-lg prose-p:mx-3  dark:prose-invert prose-li:marker:text-primary prose-a:text-primary">
+
+      <div className="mt-16 max-w-5xl lg:ml-5 max-md:px-2 prose prose-lg prose-primary">
         <PortableText
           value={data.content}
-          serializers={{
-            code: ({ node, children }) => (
-              <pre className="overflow-x-auto rounded-md bg-gray-100 p-4">
-                <code className="text-sm">{children}</code>
-              </pre>
-            ),
+          components={{
+            types: {
+              code: (value) => <div>{JSON.stringify(value)}</div>,
+            },
           }}
         />
       </div>
